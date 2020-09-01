ba974f27685d6f7770e2efb4d5d4f1de4910d364
Michał <michal.przepiorka0@gmail.com>
2020-09-01 18:13:51 +0200

    Add basic configuration for app

---
+++ src/main/java/com/contracts/BenefitsOfContractsEmploymentApplication.java
@@ -0,0 +1,13 @@
+package com.contracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitsOfContractsEmploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenefitsOfContractsEmploymentApplication.class, args);
+    }
+
+}
