16e0ebe0ebe323bf69b08b4246e1510be9e30bd7
Michał <michal.przepiorka0@gmail.com>
2020-10-28 16:53:39 +0100

    Moved ui files to the new module

--- GUI/src/main/java/com/michal/gui/Main.java
+++ GUI/src/main/java/com/michal/gui/Main.java
@@ -1,9 +1,12 @@
 package com.michal.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -12,10 +15,10 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Test");
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/panes/main-scene.fxml"));
+        primaryStage.setTitle("Main view");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 }
